7fbbf6945fa9295a2fd3873fb9cc6bcce5513135
KenIshii <riguveda44@gmail.com>
2025-04-29 15:19:22 +0900

    fix: graph

--- chpter9/Graph/main.go
+++ chpter9/Graph/main.go
@@ -9,15 +9,17 @@ func main() {
 	fmt.Scan(&N)
 	fmt.Scan(&M)
 
-	g := make([][]int, N)
+	// ノード数Nに合わせて初期化
+	g := make(Graph, N)
+	for i := range g {
+		g[i] = []int{} // 各ノードの隣接リストを初期化
+	}
 
 	for i := 0; i < M; i++ {
 		var a, b int
-		fmt.Scan(&a)
-		fmt.Scan(&b)
-
+		fmt.Scan(&a, &b)
+		// ノード番号が0始まりであることを想定
 		g[a] = append(g[a], b)
-		// 無向グラフの場合
-		g[b] = append(g[b], a)
+		g[b] = append(g[b], a) // 無向グラフならこっちも必要
 	}
 }
